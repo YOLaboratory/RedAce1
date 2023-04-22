f1363e3b115eeae55b1d94c6679b0f31d686dcaf
UsuiMM <time.000.zero@gmail.com>
2023-04-23 00:54:16 +0900

    リファクタリング

--- mars-gis-django/mars-gis-django/mars_gis_django/map3d/static/map3d/js_file/Save.js
+++ mars-gis-django/mars-gis-django/mars_gis_django/map3d/static/map3d/js_file/Save.js
@@ -1,22 +1,23 @@
-function save_spectral(counter){
-console.log("save_spectral here!!");
-console.log(data_save[counter]);
-var graph_counter = counter + 1
-var memo_counter = "save_memo_" + graph_counter.toString();
-var description = document.getElementById(memo_counter).value;
-console.log(description);
+function save_spectral(counter) {
+    console.log("save_spectral here!!");
+    console.log(data_save[counter]);
 
+    var graph_counter = counter + 1;
+    var memo_counter = "save_memo_" + graph_counter.toString();
+    var description = document.getElementById(memo_counter).value;
 
- $.ajax({
-    type: 'POST',
-    headers: { "X-CSRFToken": csrftoken },
-    url: '/spectra/spectrum/new',
-    contentType: 'application/json',
-    data: JSON.stringify({
-        "spectral_data": data_save[counter],
-        "description": description
+    console.log(description);
+
+    $.ajax({
+        type: 'POST',
+        headers: { "X-CSRFToken": csrftoken },
+        url: '/spectra/spectrum/new',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "spectral_data": data_save[counter],
+            "description": description
+        })
     })
- })
 
     alert('Save completed!!!');
 }
@@ -35,7 +36,9 @@ $(function () {
 function update_description(id){
     console.log(id)
     console.log(typeof(id))
+
     var description_new = document.getElementById(id).value;
+    
     console.log(description_new)
     console.log(typeof(description_new));
 }
