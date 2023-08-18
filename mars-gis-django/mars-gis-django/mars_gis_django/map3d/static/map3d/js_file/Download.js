9718c32cea9b217502a57193571e2f871a3dde1e
UsuiMM <time.000.zero@gmail.com>
2023-08-18 17:37:14 +0900

    サムネイル画像ウィンドウのダウンロードボタン、csvダウンロード（全ピクセル）作成

--- mars-gis-django/mars-gis-django/mars_gis_django/map3d/static/map3d/js_file/Download.js
+++ mars-gis-django/mars-gis-django/mars_gis_django/map3d/static/map3d/js_file/Download.js
@@ -1,6 +1,6 @@
 //umemo 補助情報のダウンロードボタン、4つ
 function download_xlsx_anc() {
-    var wopts = {
+    var options = {
         bookType: 'xlsx',
         bookSST: false,
         type: 'binary'
@@ -10,11 +10,11 @@ function download_xlsx_anc() {
     $('iframe').contents().find('table.ancillary').each(function (current, index) {
         var sheet_name = $('iframe').contents().find("div.infobox_name").prevObject.prevObject[0].offsetParent.textContent.slice(0, -1);
         workbook.SheetNames.push(sheet_name);
-        workbook.Sheets[sheet_name] = XLSX.utils.table_to_sheet(index, wopts);
+        workbook.Sheets[sheet_name] = XLSX.utils.table_to_sheet(index, options);
     });
 
     var filename_ancillary = $('iframe').contents().find("div.infobox_name")[0].textContent + "_" + $('iframe').contents().find("div.infobox_name").prevObject.prevObject[0].offsetParent.textContent.slice(0, -1) + ".xlsx";
-    var wbout = XLSX.write(workbook, wopts);
+    var wbout = XLSX.write(workbook, options);
     function make_xlsx(s) {
         var buf = new ArrayBuffer(s.length);
         var view = new Uint8Array(buf);
@@ -107,10 +107,12 @@ function download_pvl_anc() {
 
 //umemo スペクトルプロットの下部ダウンロードボタン、CSVダウンロード
 function download_csv_spectral(value) {
-    // console.log(Chart_list[value].file_[0]); // 最初の[x,y]
-    // console.log(Chart_list[value].file_[0].length); // 2
+    // console.log(Chart_list[value].file_[0]);              　// 最初の[x,y]
+    // console.log(Chart_list[value].file_[0].length);       // 2
+    // console.log(Chart_list[value].user_attrs_.labels[0]); // band
     // console.log(Chart_list[value].user_attrs_.labels[1]); // frt00003621_07_if166l: E:-97.65137  N:24.90353
-    // console.log(Chart_list[value].file_); // [[x,y],[x,y],...] (430)、データが少ない場合もある。
+    // console.log(Chart_list[value].file_);                　// [[x,y],[x,y],...] (430)、データが少ない場合もある。
+    // console.log(Chart_list[value].user_attrs_);           // Dygraphの設定
 
     for ( var csv_i = 1; csv_i < Chart_list[value].file_[0].length; csv_i++ ) { // crismに関してはループ1回、他は分からん、残す必要あり。
         // ダウンロード時のcsvファイル名作成
@@ -131,8 +133,9 @@ function download_csv_spectral(value) {
         for (var csv_i2 in Chart_list[value].file_) { // バンド数(x、yのペア)
             sp_csv += Chart_list[value].file_[csv_i2][0] + ",";
         }
-        sp_csv = sp_csv.slice(0, -1); //最後のコンマ除去
+        sp_csv = sp_csv.slice(0, -1); // 最後のコンマ除去
         sp_csv += "\n";
+
         // y座標(反射率)
         for (var csv_i2 in Chart_list[value].file_) { // バンド数(x、yのペア)
             if (Chart_list[value].file_[csv_i2][csv_i] != null) {
@@ -141,7 +144,7 @@ function download_csv_spectral(value) {
                 sp_csv += "-9999" + ",";
             }
         }
-        sp_csv = sp_csv.slice(0, -1); //最後のコンマ除去
+        sp_csv = sp_csv.slice(0, -1); // 最後のコンマ除去
         console.log(sp_csv);
 
         var buf = new Uint8Array([0xEF, 0xBB, 0xBF]); //umemo 型付き配列で、8ビット符号なし整数値の配列を表します, [0xEF,0xBB,0xBF]はバイトオーダマーク
@@ -149,8 +152,50 @@ function download_csv_spectral(value) {
     }
 }
 
+/**
+ * サムネイル画像ウィンドウのダウンロードボタン、csvダウンロード（全ピクセル）。
+ * @param {*} data 
+ */
+function download_csv_spectral_allpixel(data) {
+    var data_object = JSON.parse(data);
+    console.log(data_object);
 
-// サムネイル画像ウィンドウのダウンロードボタン、csvダウンロード（全ピクセル）。
-// function download_csv_spectral_allpixel(value) {
+    var filename = data_object["obs_ID"] + ".csv";
+    var sp_csv = "";
 
-// }
+    // イメージサイズ[X, Y], バンド数
+    sp_csv += data_object["Image_size"][0] + ",";
+    sp_csv += data_object["Image_size"][1] + ",";
+    sp_csv += data_object["band_number"] + "\n";
+    // sp_csv += data_object["band_number"].length + "\n";
+
+    // x座標(波長)
+    wavelength_list = data_object["band_bin_center"].split(',');
+    wavelength_list.map(Number);
+    if (wavelength_list[0] > wavelength_list[wavelength_list.length - 1]) {
+        wavelength_list.reverse();
+    }
+    for (var i = 0; i < data_object["band_number"]; i++) {
+        sp_csv += wavelength_list[i] + ",";
+    }
+    sp_csv = sp_csv.slice(0, -1); // 最後のコンマ除去
+    sp_csv += "\n";
+
+    // y座標(反射率)
+    // if (data_object["reflectance"] !== -1) {
+    //     for (var i = 0; i < data_object["reflectance"].length; i++) { // バンド数(x、yのペア)
+    //         sp_csv += wavelength_list[i] + ",";
+    //     }
+    // }
+    // for (var i = 0; i < data_object["band_number"]; i++) {
+    //     sp_csv += data_object["reflectance"][i] + "\n";
+    // }
+
+    // y座標(反射率)、1バンドのrefを全て取れる。
+    for (var i = 0; i < data_object["Image_size"][1]; i++) {
+        sp_csv += data_object["reflectance"][i] + "\n";
+    }
+
+    var buf = new Uint8Array([0xEF, 0xBB, 0xBF]);
+    saveAs(new Blob([ buf, sp_csv], { "type" : "text/csv" }), filename);
+}
